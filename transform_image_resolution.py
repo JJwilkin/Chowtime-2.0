8af715cd66265f3b4effcec33644b1e198fd3b49
Joshua Wilkinson <joshjeffreywilkinson@gmail.com>
2020-04-28 15:18:34 -0400

    oranges

--- transform_image_resolution.py
+++ transform_image_resolution.py
@@ -14,3 +14,34 @@ if __name__ == '__main__':
     parser.add_argument('-s', '--size', type=int, nargs=2, required=True, metavar=('width', 'height'), help='Image size')
     args = parser.parse_args()
     rescale_images(args.directory, args.size)
+
+"""
+import Image
+import os, sys
+
+def resizeImage(infile, output_dir="", size=(348,348)):
+     outfile = os.path.splitext(infile)[0]+"_resized"
+     extension = os.path.splitext(infile)[1]
+
+     if (cmp(extension, ".jpg")):
+        return
+
+     if infile != outfile:
+        try :
+            im = Image.open(infile)
+            im.thumbnail(size, Image.ANTIALIAS)
+            im.save(output_dir+outfile+extension,"JPEG")
+        except IOError:
+            print "cannot reduce image for ", infile
+
+
+if __name__=="__main__":
+    output_dir = "resized"
+    dir = os.getcwd()
+
+    if not os.path.exists(os.path.join(dir,output_dir)):
+        os.mkdir(output_dir)
+
+    for file in os.listdir(dir):
+        resizeImage(file,output_dir)
+"""
